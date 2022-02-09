17245f3bc72b048e987f342d04bf813d71b50482
clwater <gzb@clwater.top>
2022-02-09 14:14:50 +0800

    init(build): 初始化

---
+++ androidanimation05/app/src/main/java/com/clwater/androidanimation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.clwater.androidanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
